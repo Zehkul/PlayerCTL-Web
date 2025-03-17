6d06fc08391eca369e055195a71f34ed113a9fa5
shu <shu@lain.localdomain>
2025-03-17 01:51:07 +0100

    add hours to formattime

--- playerctl_web/static/js/player.js
+++ playerctl_web/static/js/player.js
@@ -74,9 +74,15 @@ function sendCommand(command) {
 }
 
 function formatTime(seconds) {
-  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
   const remainingSeconds = Math.floor(seconds % 60);
-  return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+
+  if (hours > 0) {
+    return `${hours}:${minutes < 10 ? '0' : ''}${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  } else {
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  }
 }
 
 function updateMetadata() {
